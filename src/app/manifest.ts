30eadb7407ec3c70d51a438fc5016a776a6a24e1
Gabriel Felipe <gabriellion97@gmail.com>
2026-03-10 16:42:17 -0300

    UP

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,23 +11,5 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#1a2744',
         orientation: 'any',
         categories: ['business', 'shopping'],
-        icons: [
-            {
-                src: '/icons/icon-192x192.png',
-                sizes: '192x192',
-                type: 'image/png',
-            },
-            {
-                src: '/icons/icon-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-            },
-            {
-                src: '/icons/icon-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'maskable',
-            },
-        ],
     }
 }
